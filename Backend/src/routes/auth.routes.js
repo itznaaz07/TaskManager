49812c893245f6919f962fd7747276da11d4140a
Ganiya Naaz <ganiyanaaz@gmail.com>
2025-10-02 07:58:11 +0530

    Update auth.routes.js

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -4,10 +4,9 @@ import {verifyJWT} from "../middlewares/auth.middlewares.js"
 const router=Router();
 
 
-//unsecured routes
 router.route("/register").post(registerUser);
 router.route("/login").post(login)
-//secure routes
+
 router.route("/profile").get(verifyJWT,getProfile);
 router.route("/logout").delete(verifyJWT,logout)
 export default router;
