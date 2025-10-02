715ac8af7ca63feb9fa0247ccef26a25763fae38
Ganiya Naaz <ganiyanaaz@gmail.com>
2025-10-02 08:04:02 +0530

    Update App.js

--- frontened/src/App.js
+++ frontened/src/App.js
@@ -4,31 +4,22 @@ import Home from "./Pages/Home";
 import SignIn from "./Pages/Login";
 import SignUp from "./Pages/Register";
 import Logout from "./Pages/Logout"; 
-import ProtectedRoute from "./Components/ProtectedRoute"; // ✅ route guard
+import ProtectedRoute from "./Components/ProtectedRoute"; 
 
 function Layout() {
   return (
-    <>
-      {/* ✅ Navbar always visible */}
-      <Navbar />
-
-      {/* ✅ Routes */}
+    <>      <Navbar />
       <Routes>
-        {/* Public routes */}
         <Route path="/register" element={<SignUp />} />
         <Route path="/login" element={<SignIn />} />
-
-        {/* Protected routes */}
         <Route
           path="/"
           element={
             <ProtectedRoute>
               <Home />
-            </ProtectedRoute>
+            
           }
         />
-
-        {/* Logout */}
         <Route path="/logout" element={<Logout />} />
       </Routes>
     </>
